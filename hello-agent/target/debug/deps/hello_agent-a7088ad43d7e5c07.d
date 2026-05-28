cc8c56ea70563171ee76aacef78d59a58c1e25c6
Josemar Carvalho <josemar@cpqd.com.br>
2026-05-27 21:49:04 -0300

    chore: removed dir hello-agent

--- hello-agent/target/debug/deps/hello_agent-a7088ad43d7e5c07.d
+++
@@ -1,5 +0,0 @@
-/home/jcarvalho/development/pessoal/ia/codex/codex-rs/mini-codex/hello-agent/target/debug/deps/hello_agent-a7088ad43d7e5c07.d: src/main.rs
-
-/home/jcarvalho/development/pessoal/ia/codex/codex-rs/mini-codex/hello-agent/target/debug/deps/hello_agent-a7088ad43d7e5c07: src/main.rs
-
-src/main.rs:
--- hello-agent/target/debug/hello-agent.d
+++
@@ -1,1 +0,0 @@
-/home/jcarvalho/development/pessoal/ia/codex/codex-rs/mini-codex/hello-agent/target/debug/hello-agent: /home/jcarvalho/development/pessoal/ia/codex/codex-rs/mini-codex/hello-agent/src/main.rs
